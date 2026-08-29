0a7ca3c981b11b582c8d2b54c28513443732488d
MohammadRushaan <mohammadrushaan84@gmail.com>
2026-08-29 23:36:21 +0530

    Square Pattern Problems

--- Patterns/SquarePattern.cpp
+++ Patterns/SquarePattern.cpp
@@ -9,17 +9,78 @@ int main()
 
     cout << " Enter size of square\n";
     cin >> size;
-    cout << " Enter character to enter\n";
-    cin >> ch;
-
+    
     for(int row=0; row<size; row++)
     {
         for(int col=0; col<size; col++)
         {
-            cout << ch << " ";
+            cout << "@ ";
         }
         cout << "\n";
     }
+
+    cout << "\n";
+
+    for(int row=0; row<size; row++)
+    {
+        for(int col=1; col<=size; col++)
+        {
+            cout << col << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+    
+    for(int row=1; row<=size; row++)
+    {
+        for(int col=1; col<=size; col++)
+        {
+            cout << row << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    for(int row=1; row<=size; row++)
+    {
+        char ch ='A';
+        for(int col=1; col<=size; col++)
+        {
+            cout << ch << " ";
+            ch=ch+1;
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    int num =1;
+    for(int row=1; row<=size; row++)
+    {   
+        for(int col=1; col<=size; col++)
+        {
+            cout << num << " ";
+            num=num+1;
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    char ch2='A';
+    for(int row=1; row<=size; row++)
+    {   
+        for(int col=1; col<=size; col++)
+        {
+            cout << ch2 << " ";
+            ch2=ch2+1;
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
 
     return 0;
 }
